f51f77e82ab9e6d743474113add503e243be9034
Vu Dinh Tuyen <tuyendinh47@gmail.com>
2016-10-21 00:14:09 +0700

    Third commit

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
 	public static void main(String[] args) {
 		system.out.println("Hello,world from GIT!");
 		System.out.println("Changes after First comit!");
+		System.out.println("Change after Pushing to remote!");
 	}
 }
 
